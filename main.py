9c3c97a224edb311654dd1dc9126f7c0623db6dd
David Lindkvist <58295825+David-Lindkvist@users.noreply.github.com>
2021-07-27 13:44:49 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,24 +1,50 @@
 # Written by David A. Lindkvist 2021-07-26
 
 # -- Tuneable variables --
-delay = 10                  
-input_pin = AnalogPin.P1
-noise_threshold = 2
-total_goal = 0
-# delay: Millisecond delay for main loop. Signal read frequency = (1000/delay) Hz
-# input_pin: MicroBit pin where sensor is connected
-# noise_threshold: Threshold for noise filter. Must be bigger then signal noise.
+delay = 10                  # Millisecond delay for main loop. Signal read frequency = (1000/delay) Hz
+input_pin = AnalogPin.P1    # MicroBit pin where sensor is connected
+noise_threshold = 2         # Threshold for noise filter. Must be bigger then signal noise.
+total_goal = 1000000        # Hold the goal for the total. When reached the game starts over.
 
-# -- Other global variables (don't change) --
-signal_base = pins.analog_read_pin(input_pin)
-total = 0
-last_value = 0
-recording = True
-# signal_base: Hold the signal value from base pressure.
-# total: Stores accumulated signal impulses.
-# last_value: Hold the previous signal value. Used to calculate signal change over time.
-# recording: Used to pause main loop
+# -- Other global variables --
+signal_base=0               # Hold the signal value from base pressure.
+total=0                     # Stores accumulated signal impulses.
+last_value=0        	    # Hold the previous signal value. Used to calculate signal change over time.
+recording=False             # Used to pause main loop
+start_time=0                # Holds the time when last game started
 
+# Run this on startup
+restart()
+def restart():
+    global signal_base
+    global total
+    global last_value
+    global recording
+    
+    recording = False
+    signal_base = pins.analog_read_pin(input_pin)
+    total = 0
+    last_value = signal_base
+    countdown(5)
+    elapsed_time = input.running_time()
+    recording = True
+
+def countdown(seconds):
+    for i in range(seconds):
+        basic.pause(1000)
+        basic.show_number(seconds - i)
+    for i in range(3):
+        basic.show_leds("""
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            """)
+        basic.clear_screen()
+        basic.pause(25)
+
+# Main loop
 def on_forever():
     global delay
     if recording:
@@ -38,7 +64,32 @@ def handle_input():
     if not area < 0:
         total += area
 
+    if total > total_goal:
+        recording = False
+        time = (input.running_time() - start_time) / 1000
+        display_goal_screen(time)
+
 def display_total():
     basic.show_string(str(total))
 
+def display_goal_screen(time):
+    for i in range(6):
+        basic.show_leds("""
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            """)
+        basic.clear_screen()
+        basic.pause(25)
+    
+    for i in range(6):
+        basic.show_number(time)
+        basic.pause(5000)
+
+def on_button_pressed_a():
+    restart()
+    
+input.on_button_pressed(Button.A, on_button_pressed_a)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,22 +1,63 @@
 //  Written by David A. Lindkvist 2021-07-26
 //  -- Tuneable variables --
 let delay = 10
+//  Millisecond delay for main loop. Signal read frequency = (1000/delay) Hz
 let input_pin = AnalogPin.P1
+//  MicroBit pin where sensor is connected
 let noise_threshold = 2
-let total_goal = 0
-//  delay: Millisecond delay for main loop. Signal read frequency = (1000/delay) Hz
-//  input_pin: MicroBit pin where sensor is connected
-//  noise_threshold: Threshold for noise filter. Must be bigger then signal noise.
-//  -- Other global variables (don't change) --
-let signal_base = pins.analogReadPin(input_pin)
+//  Threshold for noise filter. Must be bigger then signal noise.
+let total_goal = 1000000
+//  Hold the goal for the total. When reached the game starts over.
+//  -- Other global variables --
+let signal_base = 0
+//  Hold the signal value from base pressure.
 let total = 0
+//  Stores accumulated signal impulses.
 let last_value = 0
-let recording = true
-//  signal_base: Hold the signal value from base pressure.
-//  total: Stores accumulated signal impulses.
-//  last_value: Hold the previous signal value. Used to calculate signal change over time.
-//  recording: Used to pause main loop
+//  Hold the previous signal value. Used to calculate signal change over time.
+let recording = false
+//  Used to pause main loop
+let start_time = 0
+//  Holds the time when last game started
+//  Run this on startup
+restart()
+function restart() {
+    
+    
+    
+    
+    recording = false
+    signal_base = pins.analogReadPin(input_pin)
+    total = 0
+    last_value = signal_base
+    countdown(5)
+    let elapsed_time = input.runningTime()
+    recording = true
+}
+
+function countdown(seconds: number) {
+    let i: number;
+    for (i = 0; i < seconds; i++) {
+        basic.pause(1000)
+        basic.showNumber(seconds - i)
+    }
+    for (i = 0; i < 3; i++) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.clearScreen()
+        basic.pause(25)
+    }
+}
+
+//  Main loop
 function handle_input() {
+    let recording: boolean;
+    let time: number;
     
     
     
@@ -29,12 +70,40 @@ function handle_input() {
         total += area
     }
     
+    if (total > total_goal) {
+        recording = false
+        time = (input.runningTime() - start_time) / 1000
+        display_goal_screen(time)
+    }
+    
 }
 
 function display_total() {
     basic.showString("" + total)
 }
 
+function display_goal_screen(time: number) {
+    let i: number;
+    for (i = 0; i < 6; i++) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.clearScreen()
+        basic.pause(25)
+    }
+    for (i = 0; i < 6; i++) {
+        basic.showNumber(time)
+        basic.pause(5000)
+    }
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    restart()
+})
 basic.forever(function on_forever() {
     
     if (recording) {
